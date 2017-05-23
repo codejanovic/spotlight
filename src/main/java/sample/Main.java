1c2c73986aa12e4e99b8bb40c0138a546fd294be
Aleksandar Damjanovic <codejanovic@gmail.com>
2017-05-23 14:35:08 +0200

    make undecorated window moveable again

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,16 +5,34 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+        makeSceneMoveable(root, primaryStage);
+
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setTitle("Spotlight");
         primaryStage.setScene(new Scene(root, 600, 300));
         primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    private void makeSceneMoveable(Parent scene, Stage primaryStage) {
+        scene.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        scene.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+        });
     }
 
 
